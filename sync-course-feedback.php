3280b2d37b56dd0b231001483a3df17d36518244
Justin Bahati <bajustone@gmail.com>
2022-10-24 12:40:51 +0200

    sync-course-feedback

--- sync-course-feedback.php
+++ sync-course-feedback.php
@@ -2,8 +2,15 @@
 require_once("./config.php");
 header("Content-type: application/json");
 
+$method = $_SERVER["REQUEST_METHOD"];
+if($method !== "POST"){
+    $response->message = "invalid request method";
+    echo(json_encode($response));
+    die();
+}
+
 $response = new stdClass;
-$response->message = "Hello there";
+$response->method = $method;
 
 
 echo(json_encode($response));
